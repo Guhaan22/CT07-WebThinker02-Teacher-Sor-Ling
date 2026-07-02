cf7a270c65bc73c78988cf34ededbfc335db0f91
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-02 10:35:34 +0000

    Jul 2, 2026, 10:35 AM

--- L01_redCircle.js
+++ L01_redCircle.js
@@ -6,11 +6,11 @@ function setup(){
 function draw(){
     fill('red');
     noStroke();
-    ellipse(300,200,200,200);
+    ellipse(300,100,200,200);
     fill('yellow');
     noStroke();
     ellipse(300,200,200,200);
     fill('green');
     noStroke();
-    ellipse(300,200,200,200);
+    ellipse(300,300,200,200);
 }
