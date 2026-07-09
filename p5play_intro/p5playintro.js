0bddb7d601eaa6ada1d278f73877ad0015356624
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-09 10:15:08 +0000

    Jul 9, 2026, 10:15 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -11,7 +11,7 @@ function setup() {
  ball.diameter = 35;
  ball.color = "red"
   // End Basic shape testing
-square = new Sprite(100,500,)
+square = new Sprite(100,500,100.10)
    // Create a bouncing ball sprite
    // write your codes here
 
