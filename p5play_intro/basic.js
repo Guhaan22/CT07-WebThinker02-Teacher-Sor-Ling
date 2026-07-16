db812577c7f9f80ee662b57f9a66d4907835bedf
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-16 10:47:11 +0000

    Jul 16, 2026, 10:47 AM

---
+++ p5play_intro/basic.js
@@ -0,0 +1,39 @@
+function setup() {
+
+  // write your codes here
+
+  // --- Exercise: Area of Triangle ---
+  // write your codes here
+  let base = 100;
+  let height = 200;
+  let areatriangle = base * height * 0.5;
+  console.log("the area of the triangle is", areatriangle);
+
+  textSize(16);
+  text("the area of the triangle is", areatriangle, 50, 100);
+
+
+  // --- Exercise: Sum of first 10 even numbers ---
+  // write your codes here
+for (let i = 2; i <= 20; i+=2) {
+  console.log("Counting..." + i)
+  total = total + i
+}
+
+
+  // --- Exercise: Age category classification ---
+  // write your codes here
+  text("sum is"+ total, 50, 200);
+
+  // --- Exercise: Display odd numbers backward using while loop ---
+  // write your codes here
+
+  // --- Exercise: Array operations (groceries) ---
+  // write your codes here
+}
+
+
+//draw function
+function draw() {
+
+}
