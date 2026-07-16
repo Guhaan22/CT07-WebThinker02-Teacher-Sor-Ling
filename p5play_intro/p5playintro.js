dd498f07fc828e33b40f00d03321a2d49c47c8c0
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-16 09:47:04 +0000

    Jul 16, 2026, 9:47 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -32,7 +32,7 @@ function draw() {
   background(220)
 
   textSize(16)
-  text("Ball: " + round(ball.x) + "," + round(ball.y), 20,50
+  text("Ball: " + round(ball.x) + "," + round(ball.y), 20,50)
 
   if(ball.y > 600 || ball.y < 0) {
     ball.vel.y = -1 * ball.vel.y;
