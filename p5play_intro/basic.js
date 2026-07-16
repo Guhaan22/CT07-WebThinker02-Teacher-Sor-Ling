f8823aea00261a3e5a9aea8472892abfdb0eeb19
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-16 10:45:33 +0000

    Jul 16, 2026, 10:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,7 +15,7 @@ function setup() {
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i+=2) {
   console.log("Counting..." + i)
 }
 
