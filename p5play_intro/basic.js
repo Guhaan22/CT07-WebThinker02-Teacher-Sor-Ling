52a9a26f98220d21da353cdf4f6809bdcf898814
Guhaan22 <lakshmi.rbala@gmail.com>
2026-07-16 10:42:58 +0000

    Jul 16, 2026, 10:42 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,6 +15,10 @@ function setup() {
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
+for (let i = 1; i <= 10; i++) {
+  console.log("Counting..." + i)
+}
+
 
   // --- Exercise: Age category classification ---
   // write your codes here
