de1bcfa12672311d307c66bfe9ec53b1c01654d2
Guhaan22 <lakshmi.rbala@gmail.com>
2026-08-06 09:40:09 +0000

    Aug 6, 2026, 9:40 AM

---
+++ flappybird/flappybird-23jul.js
@@ -0,0 +1,25 @@
+// the final game
+let bird; floor;
+let flapMidImg, hg, base;
+
+function preload(){
+    //bird image, background, and the floor
+    flapMidIMg = loadimage("assets/yellowbird-midflap.png")
+    hg = loadimage("assets/background-day.png")
+    base =  
+}
+function setup(){
+createCanvas(600, 400);
+ background("white")
+
+ bird = new Sprite();
+ bird.x = width/2;
+ bird.y = 200;
+ bird.width = 30;
+ bird.height = 30;
+ bird.img = flapmidImg;
+}
+function draw(){ 
+    new Canvas(400, 600)
+    background("white")
+}
