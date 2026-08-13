508aa02df0e32068ce27e4bdbe56f49414b10f3f
Guhaan22 <lakshmi.rbala@gmail.com>
2026-08-13 10:19:55 +0000

    Aug 13, 2026, 10:19 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -1,6 +1,7 @@
 // write your codes here
 
 let bird, floor;
+let flapMidImg, base;
 function preload() {
 flapMidimg = loadImage("assets/yellowbird-midflap.png")
 base = loadImage("assets/base.png")
