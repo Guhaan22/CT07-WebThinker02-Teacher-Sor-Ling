8a5c2287c76072cb93e62d0cdc71b239b0a0ab75
Guhaan22 <lakshmi.rbala@gmail.com>
2026-08-13 09:52:17 +0000

    Aug 13, 2026, 9:52 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -39,6 +39,7 @@ function setup() {
     bird.img = flapMidImg;
 
     bird.collider = "static"; // "dynamic"
+    
     bird.mass = 2; // weight
     bird.drag = 0.02; // air resistance
     bird.bounciness = 0.5; // does it bounce or not?
