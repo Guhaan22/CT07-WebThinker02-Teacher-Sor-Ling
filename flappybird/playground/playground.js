f2efa71c78ac923207bf7ed91401a25f23beb6c1
Guhaan22 <lakshmi.rbala@gmail.com>
2026-08-27 09:48:50 +0000

    Aug 27, 2026, 9:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -58,7 +58,7 @@ function draw() {
         score = round(random(10,99));
     }
     else if (kb.presses("3")){
-        score = round(random())
+        score = round(random(10, 99))
     }
     
 
