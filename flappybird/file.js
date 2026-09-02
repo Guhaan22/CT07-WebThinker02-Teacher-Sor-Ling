7ae42535df29ff99bfdbb46245333ffcbc79b2c1
Guhaan22 <lakshmi.rbala@gmail.com>
2026-09-02 10:03:39 +0000

    Sep 2, 2026, 10:03 AM

---
+++ flappybird/file.js
@@ -0,0 +1,21 @@
+function setup() {
+    new Canvas(600, 400)
+    background("lightgray")
+
+    world.gravity.y = 10
+
+    floor = new Sprite();
+    floor.color = "blue"
+    floor.y = 355
+    floor.width = width;
+    floor.colider = "static"
+}
+
+function draw() {
+    background("lightgray")
+    
+    if (mouse.presses())
+        let s = new Sprite(mouseX, mouseY)
+        s.diameter = 35;
+        s.bounciness = 0.67; // under 1
+}
