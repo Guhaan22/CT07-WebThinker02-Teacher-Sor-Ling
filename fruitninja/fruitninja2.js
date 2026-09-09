eb9788b8f37b062ea418f6a2ad5a7b122e100e60
Guhaan22 <lakshmi.rbala@gmail.com>
2026-09-09 08:11:11 +0000

    Sep 9, 2026, 8:11 AM

--- fruitninja/fruitninja2.js
+++ fruitninja/fruitninja2.js
@@ -107,6 +107,7 @@ function splitFruit(x, y, fruitData) {
     left.rotationSpeed = -5;
     left.life = 60; // remove after 30 frames
     left.colider = "" // dynamic - affected by gravity and has colision/ static, not affected
+    left.overlaps
 
     // create right half
     let right = new fruitHalves.Sprite(x + 10, y, 40, 40);
