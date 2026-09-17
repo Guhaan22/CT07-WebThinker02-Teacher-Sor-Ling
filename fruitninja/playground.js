2145a95e2ac71fd5ef24c5a6ba76ccb541ff98fb
Guhaan22 <lakshmi.rbala@gmail.com>
2026-09-17 09:57:34 +0000

    Sep 17, 2026, 9:57 AM

--- fruitninja/playground.js
+++ fruitninja/playground.js
@@ -5,7 +5,8 @@ person.eyecolor = "black"
 person.wallet = 1000;
 person.race = "Chinese"
 person.gender = "male"
-person.height = 1
+person.height = 160;
+person.age = "13"
 function setup() {
     new Canvas(250,250)
     background("cyan")
